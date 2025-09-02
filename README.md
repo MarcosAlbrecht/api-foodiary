6981d50199aa96babc41efd5d73a49506f6a3659
Your Name <marcosralbrecht@gmail.com>
2025-09-01 23:46:31 -0300

    feat: list meals

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!--
 
-05:05:48
+06:48:33
 
 title: 'AWS Simple HTTP Endpoint example in NodeJS'
 description: 'This template demonstrates how to make a simple HTTP API with Node.js running on AWS Lambda and API Gateway using the Serverless Framework.'
---
+++ src/functions/listMeals.ts
@@ -0,0 +1,15 @@
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { ListMealsController } from "../controllers/ListMealsController";
+import { unauthorized } from "../utils/Http";
+import { parseProtectedEvent } from "../utils/parseProtectedEvent";
+import { parseResponse } from "../utils/parseResponse";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const request = parseProtectedEvent(event);
+    const response = await ListMealsController.handle(request);
+    return parseResponse(response);
+  } catch (error) {
+    return parseResponse(unauthorized({ error: "Invalid access token." }));
+  }
+}
