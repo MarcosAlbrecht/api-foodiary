dba4ba0a6f7dcf07bc98f234abb6cd94db1ad5b2
Your Name <marcosralbrecht@gmail.com>
2025-09-02 22:26:57 -0300

    feat: search meal by id

---
+++ src/controllers/GetMealByIdController.ts
@@ -0,0 +1,35 @@
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+import { db } from "../db";
+import { mealsTable } from "../db/schema";
+import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
+import { badRequest, ok } from "../utils/Http";
+
+const schema = z.object({
+  mealId: z.uuid(),
+});
+
+export class GetMealByIdController {
+  static async handle({
+    userId,
+    params,
+  }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(params);
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    const meal = await db.query.mealsTable.findFirst({
+      columns: {
+        id: true,
+        foods: true,
+        createdAt: true,
+        icon: true,
+        name: true,
+        status: true,
+      },
+      where: and(eq(mealsTable.userId, userId), eq(mealsTable.id, data.mealId)),
+    });
+    return ok({ meal });
+  }
+}
